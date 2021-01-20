47a802cfe6603b752a8691656cdf886b9a09d07e
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2021-01-20 11:53:58 +0100

    InfoSection styling started and in progress.

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -42,6 +42,8 @@ const HeroSection = () => {
             to="signup"
             onMouseEnter={() => onHover()}
             onMouseLeave={() => onHover()}
+            primary="true"
+            dark="true"
           >
             Get started {hover ? <ArrowForward /> : <ArrowRight />}
           </Button>
